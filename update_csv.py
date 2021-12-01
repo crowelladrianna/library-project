f7154268b5ff62a6b97e0fe9eab169311ce317c4
Adrianna Crowell <crowelladrianna@gmail.com>
2021-12-01 15:53:58 -0500

    ability to add contents of csv to complete list

--- update_csv.py
+++ update_csv.py
@@ -1,15 +1,42 @@
 import csv
 
+# eventually have these passed as command line arguments 
+# something like python update_csv.py add name_to_add
+add_file = "test_data/test_add_file.csv"
+complete_file = "test_data/test_complete_list.csv"
+
 def read_file(file_name):
     rows = []  # this list should contain one tuple per row
     
     with open(file_name, newline='') as f:
-        reader = csv.reader(f)
-        next(reader) # skips the header
+        reader = csv.DictReader(f)
+
         for row in reader:
-            row_tup = (row[0], row[1], row[2], row[3], row[4])
-            rows.append(row_tup)
+            rows.append(row)
     return rows
 
-# to_add = read_file(WHATEVER NAME)
-# complete = read_file(WHATEVER NAME)
+def parse_complete(file_name):
+    ids = set()
+    header = []
+
+    with open(file_name, newline='') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            ids.add(row[0])
+    return ids, header
+
+def add_to_complete(add_rows, complete_ids, header, complete_name):
+    with open(complete_name, 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=header)
+        for row in add_rows:
+            if(not row[header[0]] in complete_ids):
+                writer.writerow(row)
+
+    
+to_add = read_file(add_file)
+ids_present, header = parse_complete(complete_file)
+
+add_to_complete(to_add, ids_present, header, complete_file)
+
+
