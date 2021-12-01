9ec58c508ace62e0b0f632f2fab42c94aedbff21
Adrianna Crowell <crowelladrianna@gmail.com>
2021-12-01 16:02:30 -0500

    reworked adding from csv to run through csv to add only once

--- update_csv.py
+++ update_csv.py
@@ -5,16 +5,6 @@ import csv
 add_file = "test_data/test_add_file.csv"
 complete_file = "test_data/test_complete_list.csv"
 
-def read_file(file_name):
-    rows = []  # this list should contain one tuple per row
-    
-    with open(file_name, newline='') as f:
-        reader = csv.DictReader(f)
-
-        for row in reader:
-            rows.append(row)
-    return rows
-
 def parse_complete(file_name):
     ids = set()
     header = []
@@ -26,17 +16,14 @@ def parse_complete(file_name):
             ids.add(row[0])
     return ids, header
 
-def add_to_complete(add_rows, complete_ids, header, complete_name):
-    with open(complete_name, 'a', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=header)
-        for row in add_rows:
+def add_to_complete(add_name, complete_ids, header, complete_name):
+    with open(complete_name, 'a', newline='') as f1, open(add_name, newline='') as f2:
+        writer = csv.DictWriter(f1, fieldnames=header)
+        reader = csv.DictReader(f2)
+        for row in reader:
             if(not row[header[0]] in complete_ids):
                 writer.writerow(row)
 
-    
-to_add = read_file(add_file)
 ids_present, header = parse_complete(complete_file)
 
-add_to_complete(to_add, ids_present, header, complete_file)
-
-
+add_to_complete(add_file, ids_present, header, complete_file)
